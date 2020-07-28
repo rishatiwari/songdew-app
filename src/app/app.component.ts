0a1408849af348ff6d8455303754b6916d89bff3
rishatiwari <rishatiwari@outlook.com>
2020-07-28 22:41:09 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'songdew-app';
+  profileForm:FormGroup;
+  
 }
